0345b31d8561c3effc7d215b996ad4eb8b3b27c1
Kane Baccigalupi <baccigalupi@gmail.com>
2015-11-07 12:23:35 -0800

    HELM: moves helm to its own directory inside app/assets/javscripts

--- app/assets/javascripts/helm_manifest.js
+++ app/assets/javascripts/helm_manifest.js
@@ -1,20 +1,20 @@
 /*
  * Helm vendor needs:
- * = require vendor/jquery-2.1.3.js
- * = require vendor/underscore-1.8.2.js
- * = require vendor/backbone-1.1.2.js
- * = require vendor/hogan-3.0.1.js
- * = require helm.js
+ * = require helm/vendor/jquery-2.1.3.js
+ * = require helm/vendor/underscore-1.8.2.js
+ * = require helm/vendor/backbone-1.1.2.js
+ * = require helm/vendor/hogan-3.0.1.js
+ * = require helm/helm.js
  * Helm library
- * = require_tree ./request_middleware
- * = require_tree ./overrides
- * = require_tree ./mixins
- * = require_tree ./subscribers
- * = require view.js
- * = require_tree ./view
- * = require app.js
- * = require history.js
- * = require notifications.js
- * = require router.js
- * = require view_model.js
+ * = require_tree ./helm/request_middleware
+ * = require_tree ./helm/overrides
+ * = require_tree ./helm/mixins
+ * = require_tree ./helm/subscribers
+ * = require helm/view.js
+ * = require_tree ./helm/view
+ * = require helm/app.js
+ * = require helm/history.js
+ * = require helm/notifications.js
+ * = require helm/router.js
+ * = require helm/view_model.js
  */
